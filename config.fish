76e2b97cd711ab6e3a2542852edab9ff6c587f07
OmarMesqq <omarmsqt@gmail.com>
2023-09-10 13:46:27 -0300

    some changes

--- config.fish
+++ config.fish
@@ -16,8 +16,18 @@ end
 
 
 export LESSHISTFILE=-
-
+export TERM="xterm-256color"
+alias vi='vim'
+alias fs='killall stremio && killall node'
 alias ..='cl ..'
 alias r='ranger'
 alias c='clear'
 alias la='ls -A'
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+#if test -f /home/omar/.mambaforge/bin/conda
+#    eval /home/omar/.mambaforge/bin/conda "shell.fish" "hook" $argv | source
+#end
+# <<< conda initialize <<<
+
