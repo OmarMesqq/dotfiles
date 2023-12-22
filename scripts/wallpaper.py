c38362a16415019387f0aedffa3ee5387df04f87
OmarMesqq <omarmsqt@gmail.com>
2023-12-22 15:17:15 -0300

    whitespace

--- scripts/wallpaper.py
+++ scripts/wallpaper.py
@@ -4,12 +4,9 @@ import time
 # Puts current hour in a variable 
 w = int(time.strftime('%H', time.localtime()))
 
-
 if w <= 7 or w >=18:
     
     os.system('feh --bg-scale ~/wallpaper/dark.jpg')
 
 else:
     os.system('feh --bg-scale ~/j/wallpaper/bright.jpg')
-
-
