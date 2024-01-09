2e76b9b1964961b1e3284a640a491ca5ba398d05
OmarMesqq <omarmsqt@gmail.com>
2024-01-08 23:52:38 -0300

    style: comments do nvim init

--- apps/init.lua
+++ apps/init.lua
@@ -3,7 +3,7 @@ vim.cmd [[
   filetype plugin on
   filetype indent on
 ]]
-
+-- Ctrl+V for visual block
 vim.o.cursorline = true
 vim.o.incsearch = true
 vim.o.number = true
@@ -14,7 +14,12 @@ vim.o.hlsearch = true
 vim.o.showmatch = true
 vim.o.mouse = "a"
 vim.o.laststatus = 2 
+-- "*y and "*p for copying and pasting
 vim.opt.clipboard = "unnamedplus"
+-- "za" for folding/unfolding code blocks
+vim.o.foldmethod = 'syntax'   
+vim.o.foldlevel = 99          
+vim.o.foldenable = true       
 
 vim.api.nvim_set_keymap('n', 'çç', ':!pdflatex %<CR>', {noremap = true})
 
