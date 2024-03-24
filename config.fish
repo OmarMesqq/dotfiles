4179bad5ef8d1c8988ee7a2311e36dd49aa4a8a7
OmarMesqq <omarmsqt@gmail.com>
2024-03-24 20:39:53 -0300

    refactor :sparkles: add user and hostname in PS1

--- config.fish
+++ config.fish
@@ -18,17 +18,20 @@ end
 
 # Prompt
 function fish_prompt
+    set user (whoami)
+    set host (hostname)
+
     # Check if the current directory is within a Git repository
     if git rev-parse --is-inside-work-tree >/dev/null 2>&1
         # Get the Git branch and other information, keep the inner parentheses
         set git_branch (fish_git_prompt)
         # Ensure only the branch name is wrapped in parentheses
-        set git_info (string trim -c '()' $git_branch)")" 
+        set git_info (string trim -c '()' $git_branch)")"
     end
 
-    # Set prompt color and display the current directory
+    # Set prompt color and display the username@hostname, followed by the current directory
     set_color purple
-    echo -n (prompt_pwd)
+    echo -n "$user@$host: "(prompt_pwd)
 
     # If there's Git information, display it in green, followed by the prompt character
     if set -q git_info
@@ -40,7 +43,6 @@ function fish_prompt
     set_color normal
     echo -n ' $ '
 end
-
 
 export LESSHISTFILE=-
 export TERM="xterm-256color"
