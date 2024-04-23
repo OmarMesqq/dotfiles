3e7ed20f78633f96be7b8abad403a922be01bd4c
OmarMesqq <omarmsqt@gmail.com>
2024-04-23 19:17:58 -0300

    feat: :sparkles: scripts for android emulator and mounting windows

---
+++ scripts/android_emulator.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+emulator \
+-netdelay none \
+-netspeed full \
+-avd Pixel_3a_API_34_extension_level_7_x86_64 \
+-gpu host & 
---
+++ scripts/mount_windows.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo mount -o loop,offset=53477376 -t ntfs ~/j/vm/win10.img ~/j/vm/mnt 
