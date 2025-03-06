81523e2737157c0278aaf2ab5f340318c243b990
OmarMesqq <omarmsqt@gmail.com>
2025-03-06 19:08:09 -0300

    create updater script

---
+++ update_files_from_pc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ex
+
+cp -R ~/.config/alacritty/* config/alacritty 
+cp ~/.config/sway/config config/sway
+cp ~/.config/ranger/* config/ranger
+cp ~/.config/waybar/* config/waybar
+cp ~/.config/fish/config.fish config/fish
+cp ~/.config/neofetch/config.conf config/neofetch
+cp ~/.config/nvim/init.lua config/nvim
+cp ~/.config/nvim/lua/plugins.lua config/nvim/lua
+cp ~/.config/zathura/zathurarc config/zathura
+cp ~/scripts/* scripts/
