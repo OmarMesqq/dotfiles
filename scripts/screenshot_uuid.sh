1849c69671c070846bf7279ed09508efaac24e43
OmarMesqq <omarmsqt@gmail.com>
2025-03-06 19:06:18 -0300

    remove old scripts

--- scripts/screenshot_uuid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Generate a UUID and remove any hyphens to create a valid filename
-uuid=$(uuidgen | tr -d -)
-
-# Capture the screenshot using scrot with the generated UUID as the filename
-grim ~/Pictures/"${uuid}.png"
-
