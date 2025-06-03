2c8959baf5b89c97b6e6ee04abd01ae3e9c78fa1
OmarMesqq <omarmsqt@gmail.com>
2025-06-03 18:13:00 -0300

    phone backup script

---
+++ scripts/backup_phone.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+adb pull /sdcard/Android/media/com.whatsapp/ /home/omar/hdd/phone_backup   
+adb pull /sdcard/DCIM/                       /home/omar/hdd/phone_backup
+adb pull /sdcard/Download/                   /home/omar/hdd/phone_backup
+adb pull /sdcard/Music/                      /home/omar/hdd/phone_backup
+adb pull /sdcard/Pictures/                   /home/omar/hdd/phone_backup
