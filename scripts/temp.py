fbafa155d4e49b19bdd8b116b4a30bf9368a91d1
OmarMesqq <omarmsqt@gmail.com>
2025-08-28 13:35:53 -0300

    use gammastep in temp.py

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,26 +1,41 @@
 import os
+import subprocess
+import shlex
 
-STATE_FILE = "/tmp/redshift_state"
-REDSHIFT_NIGHT = "redshift -P -O 3500"
-REDSHIFT_DAY = "redshift -P -O 6000"
+STATE_FILE = "/tmp/gammastep_state"
+GAMMASTEP_NIGHT = "gammastep -P -O 3500"
+GAMMASTEP_DAY = "gammastep -P -O 6500"
 
-def toggle_redshift():
+def run(cmd):
+    cmds = shlex.split(cmd)
+    return subprocess.Popen(cmds, start_new_session=True)
+
+
+def kill():
+    return subprocess.call("killall gammastep", shell=True)
+
+
+def toggle_gammastep():
     if not os.path.exists(STATE_FILE):
         state = "night"
-        os.system(REDSHIFT_NIGHT)
+        kill()
+        run(GAMMASTEP_NIGHT)
     else:
         with open(STATE_FILE, "r") as f:
             state = f.read().strip()
 
         if state == "night":
             state = "day"
-            os.system(REDSHIFT_DAY)
+            kill()
+            run(GAMMASTEP_DAY)
         else:
             state = "night"
-            os.system(REDSHIFT_NIGHT)
+            kill()
+            run(GAMMASTEP_NIGHT)
 
     with open(STATE_FILE, "w") as f:
         f.write(state)
 
+
 if __name__ == "__main__":
-    toggle_redshift()
+    toggle_gammastep()
