3637b8446d2c91ac753e9496f95498e30172e59b
OmarMesqq <omarmsqt@gmail.com>
2025-08-28 13:35:13 -0300

    remove old scripts

--- scripts/send_to_phone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-adb push /home/omar/hdd/phone_backup/com.whatsapp/ /sdcard/Android/media/com.whatsapp/ 
-adb push /home/omar/hdd/phone_backup/DCIM /sdcard/DCIM 
-adb push /home/omar/hdd/phone_backup/Download /sdcard/Download 
-adb push /home/omar/hdd/phone_backup/Music /sdcard/Music 
-adb push /home/omar/hdd/phone_backup/Pictures /sdcard/Pictures 
-
