131f806c229b2d3adbb69e654204b822a57a4797
OmarMesqq <omarmsqt@gmail.com>
2025-12-16 19:30:19 -0300

    update updater script

--- update_files_from_pc.sh
+++ update_files_from_pc.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-set -ex
+set -x
 
 cp ~/.config/alacritty/alacritty.toml config/alacritty 
-cp ~/.config/i3/config config/i3
+cp ~/.config/sway/config config/sway
 cp ~/.config/ranger/* config/ranger
-cp ~/.config/polybar/* config/polybar
+cp ~/.config/waybar/* config/waybar
 cp ~/.config/fish/config.fish config/fish
-cp ~/.config/neofetch/config.conf config/neofetch
 cp ~/.config/nvim/init.lua config/nvim
 cp ~/.config/nvim/lua/plugins.lua config/nvim/lua
 cp ~/.config/zathura/zathurarc config/zathura
